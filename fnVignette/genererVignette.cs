a551a3a485779d19db7063ce2e55c9f8d9b3ed54
liveolivier <live.olivier.faivre@hotmail.fr>
2025-04-25 13:46:19 +0000

    Création fonction

---
+++ fnVignette/genererVignette.cs
@@ -0,0 +1,47 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Png;
+using System;
+//using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace fnVignette
+{
+    public class genererVignette
+    {
+        [FunctionName("genererVignette")]
+        public void Run([BlobTrigger("uploads/{name}", Connection = "")] Stream myBlob, string name, ILogger log,
+                        [Blob("thumbnails/{name}", FileAccess.Write)] Stream outputBlob)
+        {
+            try
+            {
+                myBlob.Position = 0;
+                AppContext.SetSwitch("System.Drawing.EnableUnixSupport", true);
+
+                log.LogInformation($"Traitement de \n Nom:{name} \n Taille: {myBlob.Length} Bytes");
+
+                // Vérifier si le fichier est un JPG
+                if (Path.GetExtension(name).Equals(".jpg", StringComparison.OrdinalIgnoreCase))
+                {
+                    using (var image = Image.Load(myBlob))
+                    {
+                        image.Mutate(x => x.Resize(320, 320));
+                        image.SaveAsJpeg(outputBlob); // Sauvegarde au format PNG
+                        log.LogInformation($"Vignette de {name} créée en JPG.");
+                    }
+                }
+                else
+                {
+                    log.LogWarning($"Le fichier {name} ne sera pas traité car non jpg.");
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"AErreur lors de la conversion de {name}: {ex.Message}");
+            }
+        }
+    }
+}
